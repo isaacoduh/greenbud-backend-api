57ebb5d46c82282e301616c08936c10235bb1828
Isaac Oduh <isaacoduh@IOs-MacBook-Pro.local>
2023-05-19 17:02:54 +0100

    install sluggable package, setup model and migration

---
+++ app/Models/Category.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+
+class Category extends Model
+{
+    use HasFactory, Sluggable;
+
+    protected $table = 'categories';
+    public $guarded = [];
+
+    protected $casts = [
+        'image' => 'json'
+    ];
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Category::class,'parent','id');
+    }
+
+    public function parent()
+    {
+        return $this->hasOne(Category::class,'id','parent');
+    }
+}
