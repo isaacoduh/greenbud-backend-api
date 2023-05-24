26273637b304bd71c07681d164ffc0d570b01acb
Isaac Oduh <isaacoduh@IOs-MacBook-Pro.local>
2023-05-24 13:04:42 +0100

    feat: login, me, logout and referesh

--- app/Http/Controllers/API/v1/UserController.php
+++ app/Http/Controllers/API/v1/UserController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\API\v1;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 
@@ -29,5 +31,56 @@ class UserController extends Controller
             'message' => 'User Created!',
             'data' => $user
         ]);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        $credentials = $request->only('email','password');
+
+        if(Auth::attempt($credentials)){
+            $user = Auth::user();
+            $token = $user->createToken('authToken')->plainTextToken;
+
+            $userData = [];
+            $userData['id'] = $user->id;
+            $userData['name'] = $user->name;
+            $userData['email'] = $user->email;
+
+            return response()->json([
+                'message' => 'Login Success!',
+                'data' => ['user' => $userData,'token' => $token]
+            ]);
+        }
+
+        throw ValidationException::withMessages([
+            'email' => ['Provided Credentials are Incorrect']
+        ]);
+    }
+
+
+    public function me(Request $request)
+    {
+        $user = $request->user();
+        return response()->json(['user' => $user]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return response()->json(['message' => 'Logged out successfully']);
+    }
+
+    public function refresh(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        $token = $request->user()->createToken('authToken')->plainTextToken;
+        
+        return response()->json(['token' => $token]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,11 +18,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
 
 Route::post('/register',[UserController::class,'register']);
+Route::post('/login',[UserController::class,'login']);
+
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/me', [UserController::class, 'me']);
+    Route::post('/logout', [UserController::class, 'logout']);
+    Route::post('/refresh', [UserController::class, 'refresh']);
+});
 
 
 
